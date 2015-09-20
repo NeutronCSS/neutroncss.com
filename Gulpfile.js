5d487d6ff31975f619a4c39f6d3f07d7429e7c01
Christopher Muller <inbox@christophermuller.net>
2015-09-20 12:35:19 +1000

    Updated gulpfile to run tasks in sequence.
    Watch now depends on browsersync, bvrowsersync now depends on bower,
    scripts, and sass.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-ruby-sass');
 var concat = require('gulp-concat');
 var bower = require('gulp-bower');
 var browserSync = require('browser-sync').create()
+var err = null; //Used for callback error tracking	 
 	  
 var config = {
 	path: {
@@ -14,47 +15,47 @@ var config = {
 	}
 }
 
-// SASS
-gulp.task('sass', function() {
+// SASS Compilation
+gulp.task('sass', function(cb) {
     return sass(config.path.sass, {style: 'expanded'})
 	.on('error', sass.logError)
     .pipe(gulp.dest(config.path.built));
+	
+	cb(err);
 });
 
-// Scripts concatenation
-gulp.task('scripts', function() {
+// Scripts Concatenation
+gulp.task('scripts', function(cb) {
   return gulp.src(config.path.js)
     .pipe(concat('app.js'))
     .pipe(gulp.dest(config.path.built));
+	
+	cb(err);
 });
 
 // Bower Setup
-gulp.task('bower', function() {
+gulp.task('bower', function(cb) {
   return bower(config.path.built)
-    .pipe(gulp.dest(config.path.built))
+    .pipe(gulp.dest(config.path.built));
+	
+	cb(err);
 });
 
 // Run browsersync server
-gulp.task('serve', ['sass', 'scripts'], function () {
-
+gulp.task('browsersync', ['sass', 'scripts', 'bower'], function (cb) {
     browserSync.init({
         server: config.path.root
     });
+	
+	cb(err);
+});
 
+// Watch tasks
+gulp.task('watch', ['browsersync'], function() {
     gulp.watch(config.path.sass, ['sass']).on('change', browserSync.reload);
     gulp.watch(config.path.js, ['scripts']).on('change', browserSync.reload);
     gulp.watch(config.path.root + '/**/*.html').on('change', browserSync.reload);
-
 });
 
 //Default task
-gulp.task('setup', ['sass', 'bower', 'scripts']);
-
-// Watch tasks
-gulp.task('watch',function() {
-    gulp.watch(config.path.sass, ['sass']);
-    gulp.watch(config.path.js, ['scripts']);
-});
-
-//Default task
-gulp.task('default', ['sass', 'scripts', 'serve', 'watch']);
+gulp.task('default', ['watch']);
