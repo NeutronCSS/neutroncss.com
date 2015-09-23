3ee451a74a8aeead089d34fde8a2d0b2bd1ee1e7
Christopher Muller <inbox@christophermuller.net>
2015-09-23 10:48:18 +1000

    Added analytics event tracking for download button.

--- assets/built/app.js
+++ assets/built/app.js
@@ -5,6 +5,24 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-9531677-15', 'auto');
 ga('send', 'pageview');
+
+
+
+// Custom event tracking
+
+// Download button
+var downloadLinks = document.querySelectorAll('download-link');
+
+function download() {
+  ga('send', 'event', 'button', 'click', 'download-link');
+}
+
+for (var i = 0; i < downloadLinks.length; ++i) {
+  var link = downloadLinks[i];
+  
+  link.addListener('click', download, false);
+}	
+
 var neutron = neutron || {};
 
 neutron.column = (function () {
