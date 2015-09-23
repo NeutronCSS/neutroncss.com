0f5619089577e47ea71eb648ba71b196931bb5d3
Christopher Muller <inbox@christophermuller.net>
2015-09-23 17:21:58 +1000

    Updated columns.js and the number of spans and the default style.
    There is still an issue that occurs if the users sets the columns to 1,
    where the margin information is lost.

--- assets/built/app.js
+++ assets/built/app.js
@@ -34,18 +34,20 @@ neutron.column = (function () {
 	column.target = "span";
 	column.container = "#flexibility";
 	column.input = document.querySelector("#column-input");
-	column.value = 12;
+	column.valueDefault = column.input.getAttribute("data-default");;
+	column.value = column.valueDefault;
 	
 	column.init = function() {
+		
 		column.input.addEventListener('keyup', changeColumn, false);
 	}
 	
 	var changeColumn = function() {
-		
+
 		setTimeout(function () {
 			var val = column.input.value.trim();
 			if(val === 0 || val === "") {
-				column.value = 12;
+				column.value = column.valueDefault;
 			} else {
 				column.value = column.input.value
 			}
@@ -57,46 +59,72 @@ neutron.column = (function () {
 	
 	var applyWidths = function() {
 		var columns = getColumns();
-				
+		
+		// Original margin values
+		var setMargin = {
+			left: 0,
+			right: 0
+		}
+		
+		// Get original margin values
+		for (var i = 0; i < columns.length; ++i) {
+			var col = columns[i];
+			
+			var marginLeft = parseFloat(window.getComputedStyle(col).getPropertyValue("margin-left"));
+			var marginRight = parseFloat(window.getComputedStyle(col).getPropertyValue("margin-right"));
+
+			if(marginLeft != 0) {
+				console.log('margin left', marginLeft);
+				setMargin.left = marginLeft;
+			}
+			
+			if(marginRight != 0) {
+				setMargin.right = marginRight;
+			}
+		}
+		
+		console.log('calculated margins: ', setMargin);
+
 		for (var i = 0; i < columns.length; ++i) {
 
 			var col = columns[i];
 			
+			// Remove any currently set styles
 			col.removeAttribute("style");
-									
+			
+			// Calc initial percentage column should take up					
 			var widthPercentage = (100 / column.value) + "%";
-			// var style = col.style;
-			var style = window.getComputedStyle(col);
 			
-			// console.log("Col's styles: ", style);
-			var marginLeft = parseFloat(style.getPropertyValue("margin-left"));
-			var marginRight = parseFloat(style.getPropertyValue("margin-right"));
+			// Get amount to compensate for flush margins		
+			var flushLeft = setMargin.left / column.value;
+			var flushRight = setMargin.right / column.value;
 			
-			// If margin is 0, use other side's margin (hacky fix)
-			marginLeft = marginLeft || marginRight;
-			marginRight = marginRight || marginLeft;
-						
-			var flushLeft = marginLeft / column.value;
-			var flushRight = marginRight / column.value;
+			// Take initial percentage and modify it by the margins and flush compensation
+			var calcedWidth = widthPercentage + " - " + setMargin.left + "px - " + setMargin.right + "px + " + flushLeft + "px + " + flushRight + "px";
 			
-			var calcedWidth = widthPercentage + " - " + marginLeft + "px - " + marginRight + "px + " + flushLeft + "px + " + flushRight + "px";
-			
+			//set width
 			col.style.width = "calc(" + calcedWidth + ")";
 			
-		}
+			// Reset styles from original CSS declaration
+			col.style.clear = "none";
+			col.style.marginLeft = setMargin.left + 'px';
+			col.style.marginRight = setMargin.right + 'px';
+		}	
 		
-		var leftMarginCols = document.querySelectorAll(column.container + " > " + column.target + ":nth-of-type(" + column.value + "n+1)");
-		var rightMarginCols = document.querySelectorAll(column.container + " > " + column.target + ":nth-of-type(" + column.value + "n+" + column.value + ")");
+		// target only the first and last columns for their specific styles
+		var newLeftMarginCols = document.querySelectorAll(column.container + " > " + column.target + ":nth-of-type(" + column.value + "n+1)");
+		var newRightMarginCols = document.querySelectorAll(column.container + " > " + column.target + ":nth-of-type(" + column.value + "n+" + column.value + ")");
 		
-		for (var i = 0; i < leftMarginCols.length; ++i) {
-			var col = leftMarginCols[i];
-			
+		// first column on a row
+		for (var i = 0; i < newLeftMarginCols.length; ++i) {
+			var col = newLeftMarginCols[i];
 			col.style.marginLeft = 0;
-		
+			col.style.clear = "left";
 		}
 		
-		for (var i = 0; i < rightMarginCols.length; ++i) {
-			var col = rightMarginCols[i];
+		// Last column on a row
+		for (var i = 0; i < newRightMarginCols.length; ++i) {
+			var col = newRightMarginCols[i];
 			col.style.marginRight = 0;
 		}		
 	}
