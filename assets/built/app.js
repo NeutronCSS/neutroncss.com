fce5a81149a10524fd630495ef6215983b06984f
Christopher Muller <inbox@christophermuller.net>
2015-10-04 15:13:27 +1100

    Updated comparison code and related JS.

--- assets/built/app.js
+++ assets/built/app.js
@@ -158,7 +158,7 @@ neutron.tab = (function () {
 
 	// locally scoped Object
 	var tab = {};
-	tab.id = '';
+	// tab.group = [];
 	
 	// return object of tab data
 	var getDataStructure = function (elements) {
@@ -198,9 +198,9 @@ neutron.tab = (function () {
 		
 	};
 	
-	var hideAllExceptActive = function(active) {
+	var hideAllExceptActive = function(active, group) {
 		console.log('Hide all except: ',active);
-		var elements = getElements();
+		var elements = getElements(group);
 		
 		var i;
 		for (i = 0; i < elements.length; ++i) {
@@ -235,11 +235,11 @@ neutron.tab = (function () {
 		}	
 	}
 	
-	var removeActiveOnTab = function() {
+	var removeActiveOnTab = function(group) {
 		//var allElements = getElements();
 		
 		// Filter elements to only one that are active
-		var attributeSelector = "[data-tabs$='.active'][data-tabs^='" + tab.id + "']";
+		var attributeSelector = "[data-tabs$='.active'][data-tabs^='" + group + "']";
 		var element = document.querySelector(attributeSelector);
 		
 		var attr = element.getAttribute("data-tabs");
@@ -252,6 +252,7 @@ neutron.tab = (function () {
 	var changeActiveTab = function() {
 		var attr = this.getAttribute("data-tabs").split('.');
 
+		var group = attr[0];
 		
 		// remove any 'active' setting if set
 		attr.splice(3, 1);
@@ -263,36 +264,36 @@ neutron.tab = (function () {
 		var activeAttribute =  attr.join('.') + '.active';
 		
 		// Deactivate active tab
-		removeActiveOnTab();
+		removeActiveOnTab(group);
 		
 		// set clicked tab to be active
 		this.setAttribute("data-tabs", activeAttribute);
 		
 		// After new active tab is set, re-hide elements
-		hideAllExceptActive(activeTab);
+		hideAllExceptActive(activeTab, group);
 		
 	}
 
-	var getElements = function() {
+	var getElements = function(group) {
 				
-		var attributeSelector = "[data-tabs^=" + tab.id + "]";
+		var attributeSelector = "[data-tabs^=" + group + "]";
 	
 		// Get matching tab elements
 		return document.querySelectorAll(attributeSelector);
 
 	}
 	
-	tab.init = function (tabId, options) {
-		tab.id = tabId;
+	tab.init = function (group, options) {
+		// tab.id = tabId;
 
 		// Get matching tab elements
-		var elements = getElements();
+		var elements = getElements(group);
 
 		// Data structure of tabs
 		var structure = getDataStructure(elements);
 
 		// Hide all pages except for designated active page
-		hideAllExceptActive(structure.active);
+		hideAllExceptActive(structure.active, group);
 		
 		// Set on click events
 		setOnClickEvents(elements);
