b49fc3bde0bce73450c9208582af05cfa717b269
Christopher Muller <inbox@christophermuller.net>
2015-10-04 14:47:12 +1100

    Updated analytics.

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,26 +1,24 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-9531677-15', 'auto');
-ga('send', 'pageview');
-
-
+// ga('create', 'UA-9531677-15', 'auto');
+// ga('send', 'pageview');
 
 // Custom event tracking
+// =====================
 
 // Download button
-var downloadLinks = document.querySelectorAll('.download-link');
-  console.log('Download links: ', downloadLinks);
-  
-function download() {
-  console.log('Download button event triggered.');
-  ga('send', 'event', 'button', 'click', 'download-link');
+var downloadLinks = document.querySelectorAll('[data-analytics=download]');
+
+function addDownloadToDatalayer() {
+	dataLayer.push({
+		'event': 'downloaded-neutron'
+	});
 }
 
 for (var i = 0; i < downloadLinks.length; ++i) {
-  var link = downloadLinks[i];
-  
-  link.addEventListener('click', download, false);
+	var link = downloadLinks[i];
+	link.addEventListener('click', addDownloadToDatalayer, false);
 }	
